69a39badcee9faf3f434789840c2159544bf236f
Aishwary200 <amritakeshri3059@gmail.com>
2021-02-04 11:47:41 +0530

    space battle

--- sketch.js
+++ sketch.js
@@ -9,7 +9,9 @@ var chances = 2;
 var p1 = true, p2 = false, p3 = false
 var gameState = "play";
 var blast;
-var pbulletImg,eBulletImg;
+var pbulletImg, eBulletImg;
+var score = 0;
+var blastSound, pBlastSound, bulletSound, eBulletSound;
 
 function preload() {
   backImg = loadImage("images/space.jpg")
@@ -18,12 +20,11 @@ function preload() {
   ebulletImg = loadImage("images/eBullet1.jpg")
   enemyImg = loadImage("images/enemy1.png")
   blast = loadImage("images/blast1.png")
+  blastSound = loadSound("sounds/blastSound.mp3")
+  pBlastSound = loadSound("sounds/pBlast.mp3")
+  bulletSound = loadSound("sounds/bulletSound.mp3")
+  eBulletSound = loadSound("sounds/eBullet.mp3")
 }
-//scales
-// playerPlane1=0.3
-// bullet=0.1
-// enemy1=0.2
-// booster=0.5
 function setup() {
   createCanvas(1000, 600)
   background = createSprite(500, 300);
@@ -42,20 +43,14 @@ function draw() {
   }
   if (gameState === "play") {
     background.velocityY = 1
-    if (keyDown("space")) {
-      playerBullet();
-    }
     spawnEnemyPlane();
     if (p1) {
       destroyPlayer(playerPlane1);
-      console.log(1)
     }
     else if (p2) {
       destroyPlayer(playerPlane2);
-      console.log(2)
     }
     else if (p3) {
-      console.log(3)
       gameState = "end";
     }
 
@@ -75,30 +70,41 @@ function draw() {
   if (gameState === "end") {
     gameOver();
   }
+
   fill("white")
   textSize(20)
   textFont("Courier New");
   text("Chances left: " + chances, 700, 50)
+  fill("white")
+  textSize(20)
+  textFont("Courier New");
+  text("Score: " + score, 100, 50)
 }
 function spawnEnemyPlane() {
   if (World.frameCount % 40 === 0) {
     var rand = Math.round(random(100, 300));
-    var r=Math.round(random(1,2))
-    switch(r){
+    var r = Math.round(random(1, 2, 3, 4))
+    switch (r) {
       case 1: enemyPlane = createSprite(1000, rand, 50, 50);
-      enemyPlane.velocityX = -5;
-      break;
+        enemyPlane.velocityX = -5;
+        break;
       case 2: enemyPlane = createSprite(0, rand, 50, 50);
-      enemyPlane.velocityX = 5;
-      break;
+        enemyPlane.velocityX = 5;
+        break;
+      case 3: enemyPlane = createSprite(500, rand, 50, 50);
+        enemyPlane.velocityX = 5;
+        break;
+      case 4: enemyPlane = createSprite(500, rand, 50, 50);
+        enemyPlane.velocityX = -5;
+        break;
       default: break;
     }
-    
+
     enemyPlane.addImage(enemyImg)
     enemyPlane.debug = true
 
     enemyPlane.scale = 0.1;
-    
+
 
     enemyGroup.add(enemyPlane)
     if (enemyPlane.y < 550 || enemyPlane.x < 900) {
@@ -112,14 +118,9 @@ function destroy(group1, group2) {
   for (var i = 0; i < group2.length; i++) {
     if (group2.get(i).isTouching(group1)) {
       group2.get(i).addImage(blast)
-      // group2.get(i).destroy();
-      // var blast1=createSprite(group2[0].position.x,group2[0].position.y,20,20)
-      // blast1.addImage(blast)
-      // blast1.scale=0.2
-      // blast1.velocityX=-2
-      console.log(group2)
-
-      //group2.addImage(blast)
+      group1.destroyEach()
+      score += 10
+      blastSound.play()
     }
   }
 }
@@ -130,8 +131,8 @@ function enemyBullet(rand) {
     eBullet = createSprite(1000, rand, 10, 10);
     eBullet.addImage(ebulletImg);
     eBullet.scale = 0.5;
-    eBullet.debug=true;
-
+    eBullet.debug = true;
+    eBulletSound.play()
 
     eBullet.velocityY = 5;
     eBullet.depth = playerPlane1.depth
@@ -149,7 +150,7 @@ function playerBullet() {
   var bullet = createSprite(150, 350, 10, 10);
   bullet.addImage(pbulletImg);
   bullet.scale = 0.3;
-
+  bulletSound.play()
   bullet.velocityY = -5;
   if (p1) {
     bullet.x = playerPlane1.x;
@@ -182,7 +183,8 @@ function keyNavigation(playerPlane) {
 function destroyPlayer(playerPlane) {
   if (eBulletGroup.isTouching(playerPlane)) {
     playerPlane.destroy();
-
+    pBlastSound.play()
+    pBulletGroup.destroyEach()
     if (playerPlane === playerPlane1) {
       p1 = false
       p2 = true
@@ -211,4 +213,9 @@ function gameOver() {
   textSize(25)
   fill("red")
   text("Game Over", 200, 200)
+}
+function keyPressed() {
+  if (keyCode === 32 && gameState === "play") {
+    playerBullet();
+  }
 }
